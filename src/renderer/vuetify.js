c9a06d1d0196c521d721ad0922a26586e8afceef
Christopher Caldwell <ccaldwell@caci.com>
2020-10-20 16:03:37 -0400

    css fixes

--- src/renderer/vuetify.js
+++ src/renderer/vuetify.js
@@ -22,21 +22,25 @@ const opts = {
         detail: '#696969',
         detailbg: '#f5f5f5',
         hover: '#f5f5f5',
-        background: '#ffffff'
+        background: '#ffffff',
+        scroll_thumb: '#808285',
+        scroll_track: '#f5f5f5'
       },
       dark: {
         main: '#192F43',
         main_active_background: '#1e1e1e',
         main_active_text: '#f5f5f5',
-        primary: '#37A5AC',
-        accent: '#76C043',
+        primary: '#4e9cca',
+        accent: '#45ced7',
         secondary: '#76C043',
         warning: '#FF8A80',
         neutral: '#414042',
-        detail: '#f5f5f5',
+        detail: '#808285',
         detailbg: '#2b2b2b',
         hover: '#414042',
-        background: '#2b2b2b'
+        background: '#2b2b2b',
+        scroll_thumb: '#f5f5f5',
+        scroll_track: '#414042'
       }
     },
     options: {
