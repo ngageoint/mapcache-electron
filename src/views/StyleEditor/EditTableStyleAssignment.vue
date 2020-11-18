90d9e7e955f61071f658fc081e60c5f6a91c1ed3
Christopher Caldwell <ccaldwell@caci.com>
2020-11-18 13:21:49 -0500

    add radio button to each list item when selecting styles to make it a little more intuitive

--- src/views/StyleEditor/EditTableStyleAssignment.vue
+++ src/views/StyleEditor/EditTableStyleAssignment.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card style="max-height: 400px;">
     <v-card-title>{{assignment.name + ' Style Assignment'}}</v-card-title>
-    <v-card-subtitle>Select a style to assign. Deselect to remove assignment.</v-card-subtitle>
+    <v-card-subtitle class="mt-1">Select a style to assign. Deselect to remove assignment.</v-card-subtitle>
     <v-card-text>
       <v-list>
         <v-list-item-group v-model="model" color="primary">
@@ -10,26 +10,30 @@
             :key="'style_' + style.id"
             link
           >
-            <v-list-item-content>
-              <v-row no-gutters justify="space-between" align="center">
-                <v-col cols="8">
-                  <v-list-item-title v-text="style.name"></v-list-item-title>
-                </v-col>
-                <v-col cols="2">
-                  <v-row no-gutters justify="center" align="center">
-                    <svg height="25" width="25" v-if="assignment.geometryType === 1">
-                      <circle cx="12.5" cy="12.5" r="5" :stroke="style.color" :fill="style.color" :stroke-width="(Math.min(style.width, 5) + 'px')"></circle>
-                    </svg>
-                    <svg height="25" width="25" v-if="assignment.geometryType === 2">
-                      <polyline points="5,20 20,15, 5,10, 20,5" :stroke="style.color" :stroke-width="(Math.min(style.width, 5) + 'px')" fill="none"></polyline>
-                    </svg>
-                    <svg height="25" width="25" v-if="assignment.geometryType === 3">
-                      <polygon points="5,10 20,5 20,20 5,20" :stroke="style.color" :fill="style.fillColor" :stroke-width="(Math.min(style.width, 5) + 'px')"></polygon>
-                    </svg>
-                  </v-row>
-                </v-col>
-              </v-row>
-            </v-list-item-content>
+            <template v-slot:default="{ active }">
+              <v-list-item-content>
+                <v-row no-gutters justify="space-between" align="center">
+                  <v-col cols="8">
+                    <v-radio-group hide-details dense class="ml-2" :value="active ? style.id : null">
+                      <v-radio class="ma-0 pa-0" dense :label="style.name" :value="style.id"></v-radio>
+                    </v-radio-group>
+                  </v-col>
+                  <v-col cols="2">
+                    <v-row no-gutters justify="center" align="center">
+                      <svg height="25" width="25" v-if="assignment.geometryType === 1">
+                        <circle cx="12.5" cy="12.5" r="5" :stroke="style.color" :fill="style.color" :stroke-width="(Math.min(style.width, 5) + 'px')"></circle>
+                      </svg>
+                      <svg height="25" width="25" v-if="assignment.geometryType === 2">
+                        <polyline points="5,20 20,15, 5,10, 20,5" :stroke="style.color" :stroke-width="(Math.min(style.width, 5) + 'px')" fill="none"></polyline>
+                      </svg>
+                      <svg height="25" width="25" v-if="assignment.geometryType === 3">
+                        <polygon points="5,10 20,5 20,20 5,20" :stroke="style.color" :fill="style.fillColor" :stroke-width="(Math.min(style.width, 5) + 'px')"></polygon>
+                      </svg>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-list-item-content>
+            </template>
           </v-list-item>
           <div v-if="assignment.geometryType === 1">
             <v-list-item
@@ -37,18 +41,22 @@
               :key="'icon' + icon.id"
               link
             >
-              <v-list-item-content>
-                <v-row no-gutters class="justify-space-between" align="center">
-                  <v-col cols="8" class="align-center">
-                    <v-list-item-title v-text="icon.name"></v-list-item-title>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-row no-gutters justify="center" align="center">
-                      <img class="icon-box" :src="icon.url"/>
-                    </v-row>
-                  </v-col>
-                </v-row>
-              </v-list-item-content>
+              <template v-slot:default="{ active }">
+                <v-list-item-content>
+                  <v-row no-gutters class="justify-space-between" align="center">
+                    <v-col cols="8">
+                      <v-radio-group hide-details dense class="ml-2" :value="active ? icon.id : null">
+                        <v-radio class="ma-0 pa-0" dense :label="icon.name" :value="icon.id"></v-radio>
+                      </v-radio-group>
+                    </v-col>
+                    <v-col cols="2">
+                      <v-row no-gutters justify="center" align="center">
+                        <img class="icon-box" :src="icon.url"/>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                </v-list-item-content>
+              </template>
             </v-list-item>
           </div>
         </v-list-item-group>
@@ -119,8 +127,8 @@
   .icon-box {
     border: 1px solid #ffffff00;
     border-radius: 4px;
-    width: 2rem;
-    height: 2rem;
+    width: 25px;
+    height: 25px;
     object-fit: contain;
   }
 </style>
