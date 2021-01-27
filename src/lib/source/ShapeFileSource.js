167e49bded30e3e407b0e4196c2adf4b8a2cbe4c
Christopher Caldwell <ccaldwell@caci.com>
2021-01-27 10:18:43 -0500

    better error handling for shapefile zip without .shp file in root directory

--- src/lib/source/ShapeFileSource.js
+++ src/lib/source/ShapeFileSource.js
@@ -14,36 +14,33 @@ export default class ShapeFileSource extends Source {
       features: []
     }
     const name = path.basename(this.filePath, path.extname(this.filePath))
-    if (path.extname(this.filePath) === '.shp') {
+    if (await jetpack.existsAsync(this.filePath)) {
       const dbfFile = path.join(path.dirname(this.filePath), name + '.dbf')
       const dbfExists = jetpack.exists(dbfFile)
       if (dbfExists) {
         featureCollection = shp.combine([shp.parseShp(jetpack.read(this.filePath, 'buffer')), shp.parseDbf(jetpack.read(dbfFile, 'buffer'))])
       } else {
-        try {
-          featureCollection.features = shp.parseShp(jetpack.read(this.filePath, 'buffer'))
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.error(error)
-        }
+        featureCollection.features = shp.parseShp(jetpack.read(this.filePath, 'buffer'))
       }
+      const { sourceId, sourceDirectory } = FileUtilities.createSourceDirectory()
+      let fileName = name + '.gpkg'
+      let filePath = path.join(sourceDirectory, fileName)
+      await GeoPackageUtilities.buildGeoPackage(filePath, name, featureCollection)
+      const extent = GeoPackageUtilities.getGeoPackageExtent(filePath, name)
+      geopackageLayers.push(new VectorLayer({
+        id: sourceId,
+        name: name,
+        geopackageFilePath: filePath,
+        sourceFilePath: this.filePath,
+        sourceDirectory: sourceDirectory,
+        sourceId: sourceId,
+        sourceLayerName: name,
+        sourceType: 'ShapeFile',
+        extent: extent
+      }))
+    } else {
+      throw new Error('.shp file not found in zip file\'s root directory.')
     }
-    const { sourceId, sourceDirectory } = FileUtilities.createSourceDirectory()
-    let fileName = name + '.gpkg'
-    let filePath = path.join(sourceDirectory, fileName)
-    await GeoPackageUtilities.buildGeoPackage(filePath, name, featureCollection)
-    const extent = GeoPackageUtilities.getGeoPackageExtent(filePath, name)
-    geopackageLayers.push(new VectorLayer({
-      id: sourceId,
-      name: name,
-      geopackageFilePath: filePath,
-      sourceFilePath: this.filePath,
-      sourceDirectory: sourceDirectory,
-      sourceId: sourceId,
-      sourceLayerName: name,
-      sourceType: 'ShapeFile',
-      extent: extent
-    }))
     return geopackageLayers
   }
 }
