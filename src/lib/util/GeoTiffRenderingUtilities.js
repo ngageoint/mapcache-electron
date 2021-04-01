d960a9cc487360576850ea6394d536b4050fa4fb
Christopher Caldwell <ccaldwell@caci.com>
2021-04-01 14:55:01 -0400

    ignore warnings for console error statements

--- src/lib/util/GeoTiffRenderingUtilities.js
+++ src/lib/util/GeoTiffRenderingUtilities.js
@@ -326,11 +326,13 @@ export default class GeoTiffRenderingUtilities {
               resolve(base64Image)
             })
           } catch (error) {
+            // eslint-disable-next-line no-console
             console.error(error)
             reject(error)
           }
         })
       } catch (e) {
+        // eslint-disable-next-line no-console
         console.error(e)
         reject(e)
       }
