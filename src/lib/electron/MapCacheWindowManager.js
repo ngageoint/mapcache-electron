061f544898d8c6609e87cee92944a6f9117b9eac
Christopher Caldwell <c.caldwell90@gmail.com>
2022-01-20 16:33:44 -0500

    update version to v1.2.0

--- src/lib/electron/MapCacheWindowManager.js
+++ src/lib/electron/MapCacheWindowManager.js
@@ -999,7 +999,7 @@ class MapCacheWindowManager {
             label: 'Documentation',
             click () {
               shell.openExternal(
-                `https://github.com/ngageoint/mapcache-electron/blob/v1.1.1/README.md`
+                `https://github.com/ngageoint/mapcache-electron/blob/v1.2.0/README.md`
               )
             }
           },
@@ -1007,7 +1007,7 @@ class MapCacheWindowManager {
             label: 'What\'s New...',
             click () {
               shell.openExternal(
-                `https://github.com/ngageoint/mapcache-electron/blob/v1.1.1/changelog/v1.1.1.md`
+                `https://github.com/ngageoint/mapcache-electron/blob/v1.2.0/changelog/v1.1.1.md`
               )
             }
           }
