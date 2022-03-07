2d93b0d17f73714ebc105b085785b8716dec818d
Christopher Caldwell <c.caldwell90@gmail.com>
2022-03-07 13:12:26 -0500

    prevent preview of wfs data sources

--- src/views/DataSources/AddDataSourceUrl.vue
+++ src/views/DataSources/AddDataSourceUrl.vue
@@ -318,7 +318,7 @@
     <div class="sticky-card-action-footer">
       <v-divider></v-divider>
       <v-card-actions>
-        <v-switch v-model="previewing" class="pl-2 mt-0" hide-details v-if="connected && dataSourceNameValid && !accessDeniedOrForbidden && !error && selectedServiceType !== 3 && (selectedDataSourceLayersValid() || selectedServiceType === 2)" label="Preview"></v-switch>
+        <v-switch v-model="previewing" class="pl-2 mt-0" hide-details v-if="connected && dataSourceNameValid && !accessDeniedOrForbidden && !error && selectedServiceType !== 1 && selectedServiceType !== 3 && (selectedDataSourceLayersValid() || selectedServiceType === 2)" label="Preview"></v-switch>
         <v-spacer></v-spacer>
         <v-btn
           text
